3e164d8efb2553207abd292dd156714b8b943f13
spacedawn96 <spacedawn96@gmail.com>
2020-08-28 04:59:27 +0900

    update auth

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const main = async () => {
       credentials: true,
     }),
   );
-
+  app.get('/', (_req, res) => res.send('mainPage'));
   app.use(cookieParser());
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
