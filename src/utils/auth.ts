17601a168b45caf3a8077afc3b66a946bcfe94ca
spacedawn96 <spacedawn96@gmail.com>
2020-08-29 04:16:52 +0900

    update

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -48,7 +48,7 @@ export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie('jid', token, {
     path: '/',
     httpOnly: true,
-    secure: false,
+    secure: true,
     domain: '.vercel-client.vercel.app',
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
   });
